a874c433beb4cac314da43b8f1d4bef011190a9e
scirelli <scirelli+git@gmail.com>
2020-05-24 21:43:08 -0400

    Setting world name at build time.

--- .docker/build.sh
+++ .docker/build.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+worldName=${1:-'Doc Ock'}
 
 docker rmi $(docker images -f "dangling=true" -q)
-docker build -t "scirelli/terraria:latest" .
+docker build \
+    --build-arg TERRARIA_WORLD_NAME="$worldName" \
+    -t "scirelli/terraria:latest" .
